f514835fe48c9e45d5a41157c8ad1a40c08e6b1f
Issacjeferry <issacjeferry2006@gmail.com>
2026-05-27 12:30:50 +0530

    Create 2679-CountDistinctNumbersOnBoard

---
+++ 2679-CountDistinctNumbersOnBoard/2679-CountDistinctNumbersOnBoard.java
@@ -0,0 +1,9 @@
+// Last updated: 5/27/2026, 12:30:50 PM
+class Solution {
+    public int distinctIntegers(int n) {
+        if(n==1){
+            return 1;
+        }
+        return n-1;
+    }
+}
