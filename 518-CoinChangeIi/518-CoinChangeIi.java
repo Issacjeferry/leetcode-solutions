01c6c6fc1f6ea295fe4b61e2d96450db4300cc99
Issacjeferry <issacjeferry2006@gmail.com>
2026-05-27 12:33:55 +0530

    Create 518-CoinChangeIi

---
+++ 518-CoinChangeIi/518-CoinChangeIi.java
@@ -0,0 +1,14 @@
+// Last updated: 5/27/2026, 12:33:54 PM
+class Solution {
+    public int change(int amount, int[] coins) {
+        int dp[] = new int[amount+1];
+        dp[0]=1;
+        for(int c:coins){
+            for(int i=c;i<=amount;i++){
+                dp[i]=dp[i]+dp[i-c];
+            }
+        }
+        return dp[amount];
+        
+    }
+}
