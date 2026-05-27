795d77abe154b97ed22ff3ffc9c6e9f39a13dbd7
Issacjeferry <issacjeferry2006@gmail.com>
2026-05-27 12:33:01 +0530

    Create 890-LemonadeChange

---
+++ 890-LemonadeChange/890-LemonadeChange.java
@@ -0,0 +1,34 @@
+// Last updated: 5/27/2026, 12:33:01 PM
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0; 
+        int ten = 0; 
+
+        for (int bill : bills) {
+            if (bill == 5) {
+                five++;
+            } 
+            else if (bill == 10) {
+                if (five > 0) {
+                    five--;
+                    ten++;  
+                } else {
+                    return false; 
+                }
+            } 
+            else { 
+                if (five > 0 && ten > 0) {
+                    five--; 
+                    ten--;  
+                } 
+                else if (five >= 3) {
+                    five -= 3; 
+                } 
+                else {
+                    return false; 
+                }
+            }
+        }
+        return true;
+    }
+}
