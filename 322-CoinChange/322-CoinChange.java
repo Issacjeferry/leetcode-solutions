990a76eaf6259fb753030f76f8335b2166368eb4
Issacjeferry <issacjeferry2006@gmail.com>
2026-05-27 12:34:46 +0530

    Create 322-CoinChange

---
+++ 322-CoinChange/322-CoinChange.java
@@ -0,0 +1,25 @@
+// Last updated: 5/27/2026, 12:34:45 PM
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int n=coins.length;
+        int dp[][] = new int[n+1][amount+1];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<amount+1;j++){
+                dp[i][j]=amount+1;
+            }
+        }
+        dp[0][0]=0;
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=amount;j++){
+                if(coins[i-1]>j){
+                    dp[i][j]=dp[i-1][j];
+                }else{
+                    dp[i][j] = Math.min(dp[i-1][j],
+                      dp[i][j-coins[i-1]]+1);
+                }
+            }
+        }
+        return (dp[n][amount])>amount ? -1:dp[n][amount];
+        
+    }
+}
