243ce4e98dbd19bed6c5e060b882b10bffb57840
Issacjeferry <issacjeferry2006@gmail.com>
2026-05-27 12:33:30 +0530

    Create 698-PartitionToKEqualSumSubsets

---
+++ 698-PartitionToKEqualSumSubsets/698-PartitionToKEqualSumSubsets.java
@@ -0,0 +1,58 @@
+// Last updated: 5/27/2026, 12:33:29 PM
+class Solution {
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        int sum = 0;
+
+        for(int n: nums){
+            sum+=n;
+        }
+
+        if(sum%k!=0){
+            return false;
+        }
+
+        int target = sum/k;
+
+        Arrays.sort(nums);
+        reverse(nums);
+
+        int[] sets = new int[k];
+
+        return backtracking(nums, 0, sets, target);
+    }
+
+    private boolean backtracking(int nums[], int index, int[] set, int target){
+        if(index==nums.length){
+            return true;
+        }
+
+        int num = nums[index];
+
+        for(int i=0; i<set.length; i++){
+            if(set[i]+num > target){
+                continue;
+            }
+
+            set[i] += num;
+            if(backtracking(nums, index+1, set, target)){
+                return true;
+            }
+            set[i] -= num;
+
+            if(set[i]==0){
+                break;
+            }
+        }
+        return false;
+    }
+
+    private void reverse(int nums[]){
+        int left = 0;
+        int right = nums.length-1;
+        while(left<right){
+            int temp = nums[left];
+            nums[left++] = nums[right];
+            nums[right--] = temp;
+        }
+    }
+}
