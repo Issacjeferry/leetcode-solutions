44a0afab0c2a5ce2cd9b7b1b3ad3fe8121b397df
Issacjeferry <issacjeferry2006@gmail.com>
2026-05-27 12:34:41 +0530

    Create 337-HouseRobberIii

---
+++ 337-HouseRobberIii/337-HouseRobberIii.java
@@ -0,0 +1,23 @@
+// Last updated: 5/27/2026, 12:34:40 PM
+class Solution {
+    public int rob(TreeNode root) {
+        int ans[] = robHouse(root);
+        return Math.max(ans[0],ans[1]);
+    }
+    
+    public int[] robHouse(TreeNode root){
+        if(root==null){
+            return new int[2];
+        }
+        
+        int left[] = robHouse(root.left);
+        int right[] = robHouse(root.right);
+        
+        int ans[] = new int[2];
+        
+        ans[0] = Math.max(left[0],left[1])+Math.max(right[0],right[1]);
+        ans[1] = root.val+left[0]+right[0];
+        
+        return ans;
+    }
+}
