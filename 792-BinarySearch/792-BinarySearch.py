318e09d11db16b87541c050e1079bb0fc2c5fbd8
Issacjeferry <issacjeferry2006@gmail.com>
2026-05-27 12:33:19 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.py
@@ -0,0 +1,15 @@
+# Last updated: 5/27/2026, 12:33:18 PM
+class Solution(object):
+    def search(self, nums, target):
+        low=0
+        high=len(nums)-1
+
+        while low <= high:
+            mid=(low+high)//2
+            if nums[mid]==target:
+                return nums.index(target)
+            elif target < nums[mid]:
+                high=mid-1
+            else :
+                low=mid+1           
+        return -1
