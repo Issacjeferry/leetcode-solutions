c1daa41dc93e5e28fc72535772416bc961cf4796
Issacjeferry <issacjeferry2006@gmail.com>
2026-05-27 12:31:56 +0530

    Create 1642-WaterBottles

---
+++ 1642-WaterBottles/1642-WaterBottles.java
@@ -0,0 +1,8 @@
+// Last updated: 5/27/2026, 12:31:56 PM
+class Solution {
+    public int numWaterBottles(int a, int b) {
+            int x=a-1;
+            int y=b-1;
+            return a+(x/y);
+    }
+}
